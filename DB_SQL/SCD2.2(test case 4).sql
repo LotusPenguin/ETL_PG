9932fdc5665695949f30d1abe5cfb38001001a7e
LotusPenguin <125930228+LotusPenguin@users.noreply.github.com>
2024-02-28 19:28:51 +0100

    Added an sql script for SCD2, test case 4

---
+++ DB_SQL/SCD2.2(test case 4).sql
@@ -0,0 +1,6 @@
+﻿USE SoftwareDB;
+
+UPDATE Programy SET Aktualna_wersja = '1.1' WHERE Nazwa_handlowa = 'DocView PDF';
+
+INSERT INTO Umowy_licencyjne (Numer_umowy, Data_zawarcia, Data_wygaśnięcia, Rodzaj_licencji, ID_Sprzedawcy, ID_Klienta, Nazwa_programu, Kwota_płatności_okresowych, Plan_płatności) VALUES
+	('LIC/31/1', '2031-01-15', '2032-01-15', 'Na osobę', 1, 1, 'DocView PDF', 100.00, 'Miesięcznie');
